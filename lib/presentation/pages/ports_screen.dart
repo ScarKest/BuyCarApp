39ba75098f52de7266fc63d9dd1b2e6b3e623122
scarkestbarrios <kestler.barrios@americavoice.com>
2022-12-28 20:48:50 -0600

    Change in ports, states and cities

--- lib/presentation/pages/ports_screen.dart
+++ lib/presentation/pages/ports_screen.dart
@@ -1,5 +1,7 @@
+// ignore_for_file: cast_nullable_to_non_nullable
 import 'package:buycar/data/datasource/local_datasource.dart';
 import 'package:buycar/domain/port.dart';
+import 'package:buycar/domain/state.dart';
 import 'package:flutter/material.dart';
 
 class PortsScreen extends StatefulWidget {
@@ -11,10 +13,17 @@ class PortsScreen extends StatefulWidget {
 
 class _PortsScreenState extends State<PortsScreen> {
   List<Port> portsList = [];
+  List<UsaState> states = <UsaState>[];
+  String user = '';
 
   Future<void> readData() async {
     portsList = await getPorts();
     setState(() {});
+  }
+
+  Future<void> _getState(String port) async {
+    setState(() {});
+    states = await getStates(port);
   }
 
   @override
@@ -25,15 +34,32 @@ class _PortsScreenState extends State<PortsScreen> {
 
   @override
   Widget build(BuildContext context) {
+    user = ModalRoute.of(context)!.settings.arguments as String;
     return Scaffold(
-      appBar: AppBar(title: const Text('Puerto'),),
+      appBar: AppBar(
+        title: const Text('Puerto'),
+      ),
       body: ListView(
         children: portsList
             .map(
               (port) => ListTile(
                 leading: const Icon(Icons.anchor_rounded),
                 title: Text(port.puerto),
-                onTap: () => Navigator.pop(context, port),
+                onTap: () {
+                  setState(() {
+                    _getState(port.puerto.toLowerCase()).then(
+                      (value) => Navigator.pushNamed(
+                        context,
+                        '/states',
+                        arguments: {
+                          'States': states,
+                          'Port': port.puerto,
+                          'User': user,
+                        },
+                      ).then((value) => Navigator.pop(context, value)),
+                    );
+                  });
+                },
               ),
             )
             .toList(),
--- lib/presentation/widgets/ship_widget.dart
+++ lib/presentation/widgets/ship_widget.dart
@@ -1,9 +1,7 @@
 // ignore_for_file: use_build_context_synchronously
-import 'package:buycar/data/datasource/local_datasource.dart';
 import 'package:buycar/domain/barco.dart';
 import 'package:buycar/domain/city.dart';
 import 'package:buycar/domain/port.dart';
-import 'package:buycar/domain/state.dart';
 import 'package:buycar/presentation/widgets/list_tile_custom.dart';
 import 'package:flutter/material.dart';
 
@@ -20,23 +18,20 @@ class _ShipWidgetState extends State<ShipWidget> {
   //Variables para obtener los datos
   Ship ship = Ship(tipo: '', precio: 0);
   Port port = Port(puerto: '');
-  UsaState state = UsaState(estado: '');
-  List<UsaState> states = [];
-  List<City> cities = [];
   City city = City(ciudad: '', precio: 0);
 
   //Variables para calcular
   int? total = 0;
 
-  Future<List<UsaState>> _getState(String port) {
-    setState(() {});
-    return getStates(port);
-  }
+  // Future<List<UsaState>> _getState(String port) {
+  //   setState(() {});
+  //   return getStates(port);
+  // }
 
-  Future<List<City>> _getCities(String port, String state) {
-    setState(() {});
-    return getCities(port, state);
-  }
+  // Future<List<City>> _getCities(String port, String state) {
+  //   setState(() {});
+  //   return getCities(port, state);
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -51,55 +46,57 @@ class _ShipWidgetState extends State<ShipWidget> {
           subTitle: '${ship.tipo} \$${ship.precio}',
         ),
         ListTileCustom(
-          title: 'Puerto',
-          onTap: () => Navigator.pushNamed(context, '/ports').then(
-            (value) => setState(() => port = value! as Port),
-          ),
-          subTitle: port.puerto,
-        ),
-        ListTileCustom(
-          title: 'Estado',
-          onTap: () async {
-            if (port.puerto.isNotEmpty) {
-              states = await _getState(port.puerto.toLowerCase());
-              await Navigator.pushNamed(context, '/states', arguments: states)
-                  .then(
-                (value) => setState(() => state = value! as UsaState),
-              );
-            } else {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('Seleccione un puerto primero')),
-              );
-            }
-          },
-          subTitle: state.estado,
-        ),
-        ListTileCustom(
           title: 'Ciudad',
-          onTap: () async {
-            if (state.estado.isNotEmpty) {
-              cities =
-                  await _getCities(port.puerto.toLowerCase(), state.estado);
-              await Navigator.pushNamed(
-                context,
-                '/cities',
-                arguments: {
-                  'Cities': cities,
-                  'User': widget.user,
-                },
-              ).then(
-                (value) => setState(() => city = value! as City),
-              );
-            } else {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Text('Seleccione un estado primero'),
-                ),
-              );
-            }
-          },
+          onTap: () =>
+              Navigator.pushNamed(context, '/ports', arguments: widget.user)
+                  .then(
+            (value) => setState(() => city = value! as City),
+          ),
           subTitle: '${city.ciudad} \$${city.precio}',
         ),
+        // ListTileCustom(
+        //   title: 'Estado',
+        //   onTap: () async {
+        //     if (port.puerto.isNotEmpty) {
+        //       states = await _getState(port.puerto.toLowerCase());
+        //       await Navigator.pushNamed(context, '/states', arguments: states)
+        //           .then(
+        //         (value) => setState(() => state = value! as UsaState),
+        //       );
+        //     } else {
+        //       ScaffoldMessenger.of(context).showSnackBar(
+        //         const SnackBar(content: Text('Seleccione un puerto primero'))
+        //       );
+        //     }
+        //   },
+        //   subTitle: state.estado,
+        // ),
+        // ListTileCustom(
+        //   title: 'Ciudad',
+        //   onTap: () async {
+        //     if (state.estado.isNotEmpty) {
+        //       cities =
+        //           await _getCities(port.puerto.toLowerCase(), state.estado);
+        //       await Navigator.pushNamed(
+        //         context,
+        //         '/cities',
+        //         arguments: {
+        //           'Cities': cities,
+        //           'User': widget.user,
+        //         },
+        //       ).then(
+        //         (value) => setState(() => city = value! as City),
+        //       );
+        //     } else {
+        //       ScaffoldMessenger.of(context).showSnackBar(
+        //         const SnackBar(
+        //           content: Text('Seleccione un estado primero'),
+        //         ),
+        //       );
+        //     }
+        //   },
+        //   subTitle: '${city.ciudad} \$${city.precio}',
+        // ),
         ElevatedButton(
           onPressed: () => setState(
             () => total = ship.precio + city.precio,
